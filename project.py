793c42006fea89534e33ba6f7fc480aa686fe287
CookeRichard94 <g00331787@gmit.ie>
2019-03-27 17:06:47 +0000

    Commit 4 - Began Intergration of Thompson's Construction

--- project.py
+++ project.py
@@ -15,18 +15,23 @@ def shntYrdAlg(infix):
     # . = concatenate
     # | = or
  
-    pofix = ""
-    stack = ""
+    pofix, stack = "", ""
 
     # For loop to loop through each character
     # of the infix string one by one
     for c in infix:
+        #If the character at c is an ( then add it to the stack
         if c =='(':
             stack = stack + c
+        #if the character at c is an ) and while the last character on the stack
+        # is not a ( then pop everything from the stack on to the pofix
+        # otherwise 
         elif c ==')':
             while stack[-1] != '(':
                 pofix, stack = pofix + stack[-1], stack[:-1]
             stack = stack[:-1]
+        # if the character at c is in the specialChars library and it's prcedence 
+        # is lower <= to the last chaarcter in the stack, then pop 
         elif c in specialChars:
             while stack and specialChars.get(c, 0) <= specialChars.get(stack[-1], 0):
                 pofix, stack = pofix + stack[-1], stack[:-1]
@@ -39,6 +44,35 @@ def shntYrdAlg(infix):
 
     return pofix
 
+#
 print (shntYrdAlg("(a.b)|(c*.d)"))
 print (shntYrdAlg("(a.*b)|(c*d)"))
 print (shntYrdAlg("(a*b)|(c.d)"))
+
+
+#Thompson's Construction
+#
+class state:
+    label, arrow1, arrow2 = None, None, None
+
+#
+class nfa:
+    initial, accept = None, None
+
+    def __init__(self, initial, accept):
+        self.initial, self.accept = initial, accept
+
+def compile(pofix):
+    nfaStack = []
+
+    for c in pofix:
+        accept, initial = state(), state()
+        initial.label = c
+
+        if
+
+        elif
+
+        elif
+
+        else

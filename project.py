796a3557f58ba9ea81832f9b05294ae636def62c
CookeRichard94 <g00331787@gmit.ie>
2019-03-27 21:06:52 +0000

    Commit 8 - Thompson's Complete(Without '?'), testcase included

--- project.py
+++ project.py
@@ -15,12 +15,14 @@ def shntYrdAlg(infix):
     #and their order of precedence
     specialChars = {'*': 25, '+':20, '?':15,'.':10, '|':5}
     # * = 0 or more
-    # * = 1 or more
-    # ? = 
+    # + = 1 or more
+    # ? = 0 or 1
     # . = concatenate
     # | = or
  
-    #
+    #postfix and stack are initially created as empty strings
+    #the stack is used to temporarily hold characters to be pushed to the pofix
+    #The pofix is used as the output for the Shunting yard algorithm
     pofix, stack = "", ""
 
     # For loop to loop through each character
@@ -33,21 +35,27 @@ def shntYrdAlg(infix):
         # is not a ( then pop everything from the stack on to the pofix
         # otherwise 
         elif c ==')':
+            #while loop that runs while the last character on the stack is not a (
             while stack[-1] != '(':
                 pofix, stack = pofix + stack[-1], stack[:-1]
             stack = stack[:-1]
         # if the character at c is in the specialChars library and it's prcedence 
         # is lower <= to the last chaarcter in the stack, then pop 
         elif c in specialChars:
+            #while loop that runs while the current special character has a lower
+            #precedence than the special character on the stack
             while stack and specialChars.get(c, 0) <= specialChars.get(stack[-1], 0):
                 pofix, stack = pofix + stack[-1], stack[:-1]
             stack = stack + c
+        #
         else:
             pofix = pofix + c
 
+    #
     while stack:
         pofix, stack = pofix + stack[-1], stack[:-1]
 
+    #returns the corrected regular expression in postfix
     return pofix
 
 # Potential test cases to check if the Shunting yard 
@@ -61,19 +69,26 @@ print (shntYrdAlg("(a*b)|(c.d)"))
 
 
 #Thompson's Construction
+#https://cs.stackexchange.com/questions/76488/thompsons-construction-transforming-a-regular-expression-into-an-equivalent-nf
 
 #Each one reperesents a state that has two arrows
 #None is used for a label to represent an empty set for the arrows
 class state:
+    #Variables for the state class
+    #None is used in python as a way of saying the the value of a variable has not been assigned yet
     label, arrow1, arrow2 = None, None, None
 
 # Each NFA contains and intial and an accept state
 class nfa:
+    #Variables in the nfa class
     initial, accept = None, None
 
+    #constructor for building a new nfa
     def __init__(self, initial, accept):
         self.initial, self.accept = initial, accept
 
+#Method that takes in a regular expression and turns them into a 
+#data structure(NFA)
 def compile(pofix):
     nfaStack = []
 
@@ -89,7 +104,7 @@ def compile(pofix):
             #Joins the old accept state to the new accept state and to nfa1's intial state
             nfa1.accept.arrow1, nfa1.accept.arrow2 = nfa.initial, accept
             #Pushes the new NFA to the stack
-            nfastack.append(nfa(initial, accept))
+            nfaStack.append(nfa(initial, accept))
         elif c == '.':
             #Pops 2 NFA's off the stack
             nfa2, nfa1 = nfaStack.pop(), nfaStack.pop()
@@ -97,7 +112,7 @@ def compile(pofix):
             #Initial state
             nfa1.accept.arrow1 = nfa2.initial
             #Pushes the new NFA to the stack
-            nfaStack.append(nfa1.initial, nfa2.accept))
+            nfaStack.append(nfa(nfa1.initial, nfa2.accept))
         elif c == '|':
             #Pops 2 NFA's off the stack
             nfa2, nfa1 = nfaStack.pop(), nfaStack.pop()
@@ -111,7 +126,7 @@ def compile(pofix):
             #Connects the new Accept state to the two NFA's popped from the stack
             nfa1.accept.arrow1, nfa2.accept.arrow1 = accept, accept
             #Pushes the new NFA to the stack
-            nfastack.append(nfa(initial, accept))
+            nfaStack.append(nfa(initial, accept))
         elif c == '+':
             #Pops a single NFA from the stack
             nfa1 = nfaStack.pop()
@@ -122,11 +137,17 @@ def compile(pofix):
             #Joins the old accept state to the new accept state and to nfa1's intial state
             nfa1.accept.arrow1, nfa1.accept.arrow2 = nfa.initial, accept
             #Pushes the new NFA to the stack
-            nfastack.append(nfa(initial, accept))
+            nfaStack.append(nfa(initial, accept))
         else:
             #Creates new accept and initial states
             accept, initial = state(), state()
             #Joins the initial state to the accept state using an arrow that is labbelled c
             initial.label, initial.arrow1 = c, accept
             ##Pushes the new NDA to the stack
-            nfastack.append(nfa(initial, accept))
+            nfaStack.append(nfa(initial, accept))
+        
+    #Should only have a single NFA
+    return nfaStack.pop()
+
+#expected output is a .nfa at a specified memory location
+print(compile("ab.cd.|"))
